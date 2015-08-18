5d7a4192f87a16979d977b7c4f58980a4a60b61d
weizheng@google.com <weizheng@google.com>
2015-08-18 13:17:49 -0700

    Deploy default module if not exists. Fix command parameters to make it run with latest version of gcloud

--- install.sh
+++ install.sh
@@ -20,23 +20,78 @@ VERSION=1
 # Local tag for the Docker image we deploy
 DOCKER_TAG="google/phabricator-appengine"
 
+function defaultModuleExists() {
+  local project=$1
+  local count=$(gcloud preview app modules list default --project $project 2>&1 \
+    | grep -o "^default" | wc -l)
+  if [[ $count -gt 0 ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+function deployDefaultModule() {
+  local target_project=$1
+  local tmp_dir=$(mktemp -d --tmpdir=$(pwd))
+  echo
+  echo "Default module doesn't exist, deploying default module now..."
+  cat > ${tmp_dir}/app.yaml <<EOF
+module: default
+runtime: python27
+api_version: 1
+threadsafe: true
+handlers:
+- url: /
+  mime_type: text/html
+  static_files: hello.html
+  upload: (.*html)
+EOF
+
+  cat > ${tmp_dir}/hello.html <<EOF
+<html>
+  <head>
+    <title>Sample Hello-World Page.</title>
+  </head>
+  <body>
+    Hello, World!
+  </body>
+</html>
+EOF
+  local status=1
+  gcloud preview app deploy --force --quiet --project $target_project \
+    $tmp_dir/app.yaml --version v1
+  [[ $? ]] && status=0 || echo "Failed to deploy default module to $target_project"
+  rm -rf $tmp_dir
+  return $status
+}
+
+if ! defaultModuleExists $PROJECT; then
+  if ! deployDefaultModule $PROJECT; then
+    exit
+  fi
+fi
+
 # Ensure that we have a local image to deploy
 if [ -z "$(docker images -q --all ${DOCKER_TAG})" ]; then
   docker pull gcr.io/developer_tools_bundle/bundle-phabricator:latest
   docker tag gcr.io/developer_tools_bundle/bundle-phabricator:latest ${DOCKER_TAG}
 fi
 
+
 # Ensure that a Cloud SQL instance exists
 if [ -z "$(gcloud --project=${PROJECT} sql instances list | grep phabricator)" ]; then
-  gcloud --project="${PROJECT}" sql instances create "phabricator" \
+  gcloud --quiet --project="${PROJECT}" sql instances create "phabricator" \
     --backup-start-time="00:00" \
     --require-ssl \
     --assign-ip \
     --authorized-networks "0.0.0.0/0" \
     --tier="D1" \
-    --pricing-plan="PACKAGES" \
-    --database-flags="sql_mode=STRICT_ALL_TABLES,ft_min_word_len=3"
+    --pricing-plan="PACKAGE" \
+    --database-flags="sql_mode=STRICT_ALL_TABLES" \
+    --database-flags="ft_min_word_len=3"
 fi
+
 INSTANCE_NAME=$(gcloud --project="${PROJECT}" sql instances list | grep phabricator | cut -d " " -f 1)
 
 # Ensure that a private networks exists for Phabricator
